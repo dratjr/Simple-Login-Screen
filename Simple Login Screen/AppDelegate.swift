2c7344d0a53a3d277eb3b17f1f79b0736537d8b0
dratjr <dratjr94@gmail.com>
2017-02-15 14:38:55 -0500

    First commit
    Simple Login Screen. Text fields stored in array

--- Simple Login Screen/AppDelegate.swift
+++ Simple Login Screen/AppDelegate.swift
@@ -14,8 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let controllerId = "Login";
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: controllerId) as UIViewController
+        self.window?.rootViewController = initViewController
+        
         return true
     }
 
---
+++ Simple Login Screen/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  Simple Login Screen
+//
+//  Created by David Ratliff on 2/15/17.
+//  Copyright © 2017 Login Screen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
+    
+    var emailArray: [String] = []
+    var usernameArray: [String] = []
+    var passwordArray: [String] = []
+
+    
+    @IBAction func signupButtonPressed(_ sender: Any) {
+        
+        emailArray.append(emailTextField!.text!)
+        usernameArray.append(usernameTextField!.text!)
+        passwordArray.append(passwordTextField!.text!)
+        
+        print(emailArray[0])
+        
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
