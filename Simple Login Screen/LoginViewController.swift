96c362a4beb60cdac6e9fc4cf07bea1370ae26a6
Joshua Williams <Josh@Joshs-MacBook-Pro.local>
2017-02-19 23:32:09 -0500

    More Instructions

--- Simple Login Screen/LoginViewController.swift
+++ Simple Login Screen/LoginViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+/**
+ * 1. Make sure users cannot add the same email or username twice
+ * 2. Check if the email is a valid email. (regex)
+ * 3. Check if user name is valid.
+ * 4. Ensure password is strong (Needs a number and special character and needs to be at least 8 characters)
+ * 5. Print log statements for each failure and success case
+ **/
+
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var emailTextField: UITextField!
@@ -21,13 +29,9 @@ class LoginViewController: UIViewController {
 
     
     @IBAction func signupButtonPressed(_ sender: Any) {
-        
         emailArray.append(emailTextField!.text!)
         usernameArray.append(usernameTextField!.text!)
         passwordArray.append(passwordTextField!.text!)
-        
-        print(emailArray[0])
-        
     }
     
     
